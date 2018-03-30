5bb092438357239c6c621af9d7a5c414fba70c9c
pmnorris <pnorris@ycp.edu>
2018-03-30 15:16:15 -0400

    Organized classes

--- Shopeze test/src/edu/ycp/cs320/Shopeze/main/Main.java
+++ Shopeze test/src/edu/ycp/cs320/Shopeze/main/Main.java
@@ -1,101 +1,32 @@
 package edu.ycp.cs320.Shopeze.main;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.File;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.Timer;
+import org.eclipse.jetty.server.Server;
 
-import edu.ycp.cs320.Shopeze.controller.ShopezeController;
-import edu.ycp.cs320.Shopeze.model.ShopezeModel;
-import edu.ycp.cs320.Shopeze.model.Account;
-import edu.ycp.cs320.Shopeze.model.Aisle;
-import edu.ycp.cs320.Shopeze.model.GroceryList;
-import edu.ycp.cs320.Shopeze.model.Guest;
-import edu.ycp.cs320.Shopeze.model.Item;
-import edu.ycp.cs320.Shopeze.model.Location;
-import edu.ycp.cs320.Shopeze.model.Route;
-
-
-public class ShopezeView extends JPanel {
-	
-	private ShopezeModel model;
-	private GameController controller;
-	
-	public GameView(Game model) {
-		this.model = model;
-		setPreferredSize(new Dimension((int) model.getWidth(), (int)model.getHeight()));
-		setBackground(MIDNIGHT_RED);
-	}
-	
-	public void setController(GameController controller) {
-		this.controller = controller;
-	}
-	
-	public void startAnimation() {
-		timer.start();
-	}
-
-	protected void handleTimerTick() {
-		if (controller == null) {
-			return;
-		}
-		Square square = model.getSquare();
-		Point mouseLoc = getMousePosition();
-		if (mouseLoc != null) {
-			controller.computeSquareMoveDirection(model, square, mouseLoc.getX(), mouseLoc.getY());
-		}
-		controller.moveSquare(model, square);
-		repaint();
-	}
-	
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g); // paint background
+public class Main {
+	public static void main(String[] args) throws Exception {
+		String webappCodeBase = "./war";
+		File warFile = new File(webappCodeBase);
+		Launcher launcher = new Launcher();
 		
-		// djh2-KEC110-21: changed from GREEN to RED
-		// djh2-YCPlaptop: change from RED to YELLOW
-		g.setColor(Color.MAGENTA);
+		// get a server for port 8081
+		System.out.println("CREATING: web server on port 8081");
+		Server server = launcher.launch(true, 8081, warFile.getAbsolutePath(), "/dhenry5");
 
-		Square square = model.getSquare();
+        // Start things up!		
+		System.out.println("STARTING: web server on port 8081");
+		server.start();
 		
-		g.fillRect((int) square.getX(), (int) square.getY(), (int) square.getWidth(), (int) square.getHeight());
-	}
-	
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				Game model = new Game();
-				model.setWidth(640.0);
-				model.setHeight(480.0);
-				
-				Square square = new Square();
-				square.setX(300.0);
-				square.setY(220.0);
-				square.setWidth(40.0);
-				square.setHeight(40.0);
-				model.setSquare(square);
-				
-				GameController controller = new GameController();
-				
-				GameView view = new GameView(model);
-				view.setController(controller);
-				
-				JFrame frame = new JFrame("Move the Square!");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.add(view);
-				frame.pack();
-				frame.setVisible(true);
-				
-				view.startAnimation();
-			}
-		});
+		// dump the console output - this will produce a lot of red text - no worries, that is normal
+		server.dumpStdErr();
+		
+		// Inform user that server is running
+		System.out.println("RUNNING: web server on port 8081");
+		
+        // The use of server.join() the will make the current thread join and
+        // wait until the server is done executing.
+        // See http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
+		server.join();
 	}
 }
